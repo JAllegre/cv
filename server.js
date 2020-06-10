3699129c21f61314d44e0f3288d3e5f4a6652fa7
JAllegre <ju.allegre@gmail.com>
2020-06-10 17:20:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
+const PORT = 80;
 const HOST = '0.0.0.0';
 
 // App
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, HOST);
+app.listen(PORT/*, HOST*/);
 console.log(`Running on http://${HOST}:${PORT}`);
