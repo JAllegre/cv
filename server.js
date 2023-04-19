c32cbe42a26b9d92303292917950e38c5c147a3d
Julien Allègre <ju.allegre@gmail.com>
2023-04-19 21:35:26 +0200

    sms

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
 const express = require('express');
+const twilio = require('twilio');
 
-// Constants
-const PORT = process.env.PORT || 8080;
-const HOST = '0.0.0.0';
+const TWILIO_ACCOUNT_SID = 'AC785029671434993c4d626d4957e5a983';
+const TWILIO_AUTH_TOKEN = 'ba96ee632a0e0ba16ad8fe319c3cfe87';
+const TWILIO_FROM_ALPHA_SENDERID = 'VIABEEZ';
+const PORT = process.env.PORT || 80;
 
-// App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.use('/', (req, res) => {
-  res.send('Hello World');
-});
+app.get('/go', (req, res) => {
+  const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, { logLevel: 'debug' });
+  const body = `Rappel: PAYER TRANSACTIONS`;
+  const to = '+33785310314';
 
-app.use(function(err, req, res, next) {
+  client.messages
+    .create({
+      body,
+      from: TWILIO_FROM_ALPHA_SENDERID,
+      to
+    })
+    .then((message) => {
+      console.log('SUCCESS', message);
+    })
+    .catch((err) => {
+      console.log('ERROR', err);
+    });
+
+  res.send('go');
+});
+app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
 
 app.listen(PORT);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on ${PORT}`);
