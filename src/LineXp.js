0e84f6706102930caec8bc6a1ebc062a93dc06e3
Julien Allegre <julien@myli.io>
2023-09-22 10:44:07 +0200

    refactor

---
+++ src/LineXp.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { injectIntl } from 'react-intl';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  line: {
+    display: 'flex',
+    '@media (max-width: 400px)': {
+      flexDirection: 'column'
+    }
+  },
+  linePeriod: {
+    flex: '0 0 160px',
+    textAlign: 'left',
+    whiteSpace: 'nowrap',
+    color: '#878787',
+    fontWeight: 'bold',
+    '@media (max-width: 400px)': {
+      flex: '0 0 auto',
+      marginBottom: '10px'
+    }
+  },
+  lineContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    padding: '0 0 0 20px',
+    '& ul': {
+      paddingInlineStart: '30px'
+    },
+    '@media (max-width: 600px)': {
+      padding: '0 0 0 0'
+    }
+  },
+  lineRole: {
+    marginBottom: '10px',
+    fontWeight: 'bold'
+  },
+  lineLocation: {
+    //marginBottom: '6px',
+    color: '#878787'
+  },
+  lineText: {
+    marginBottom: '0',
+    textAlign: 'left',
+    '& li': {
+      paddingBottom: '9px'
+    },
+    '& ul': {
+      marginTop: '4px'
+    }
+  },
+  lineNotes: {
+    textAlign: 'left',
+    margin: '0 0 30px 0',
+    color: '#878787'
+  }
+});
+
+function formatXpText(str = '') {
+  return {
+    __html: '<ul><li> ' + str.replace(/\.\. /g, '</li><li>') + '</li></ul>'
+  };
+}
+
+function LineXp({ id, intl }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.line}>
+      <div className={classes.linePeriod}>{intl.formatMessage({ id: `str.${id}.period` })}</div>
+      <div className={classes.lineContent}>
+        <div style={{ display: 'flex' }}>
+          <div className={classes.lineRole}>{intl.formatMessage({ id: `str.${id}.role` })}</div>
+          <div className={classes.lineLocation}>
+            {'\u00A0 \u00A0'} @{intl.formatMessage({ id: `str.${id}.location` })}
+          </div>
+        </div>
+        <div className={classes.lineText} dangerouslySetInnerHTML={formatXpText(intl.formatMessage({ id: `str.${id}.text` }))} />
+        <div className={classes.lineNotes}>{intl.formatMessage({ id: `str.${id}.notes` })}</div>
+      </div>
+    </div>
+  );
+}
+
+export default injectIntl(LineXp);
--- src/SectionXp.js
+++ src/SectionXp.js
@@ -6,90 +6,14 @@ import PrintPageBreaker from './PrintPageBreaker';
 import Section from './Section';
 import SectionContent from './SectionContent';
 import SectionTitle from './SectionTitle';
+import LineXp from './LineXp';
 
 const useStyles = createUseStyles({
   content: {
     display: 'flex',
     flexDirection: 'column'
-  },
-  line: {
-    display: 'flex',
-    '@media (max-width: 400px)': {
-      flexDirection: 'column'
-    }
-  },
-  linePeriod: {
-    flex: '0 0 100px',
-    textAlign: 'left',
-    whiteSpace: 'nowrap',
-    color: '#878787',
-    fontWeight: 'bold',
-    '@media (max-width: 400px)': {
-      flex: '0 0 auto',
-      marginBottom: '10px'
-    }
-  },
-  lineContent: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    padding: '0 0 0 20px',
-    '& ul': {
-      paddingInlineStart: '30px'
-    },
-    '@media (max-width: 600px)': {
-      padding: '0 0 0 0'
-    }
-  },
-  lineRole: {
-    marginBottom: '10px',
-    fontWeight: 'bold'
-  },
-  lineLocation: {
-    //marginBottom: '6px',
-    color: '#878787'
-  },
-  text: {
-    marginBottom: '0',
-    textAlign: 'left',
-    '& li': {
-      paddingBottom: '9px'
-    },
-    '& ul': {
-      marginTop: '4px'
-    }
-  },
-  notes: {
-    textAlign: 'left',
-    margin: '0 0 30px 0',
-    color: '#878787'
   }
 });
-
-function formatXpText(str = '') {
-  return {
-    __html: '<ul><li> ' + str.replace(/\.\. /g, '</li><li>') + '</li></ul>'
-  };
-}
-
-function Line({ period, role, location, text, notes }) {
-  const classes = useStyles();
-  return (
-    <div className={classes.line}>
-      <div className={classes.linePeriod}>{period}</div>
-      <div className={classes.lineContent}>
-        <div style={{ display: 'flex' }}>
-          <div className={classes.lineRole}>{role}</div>
-          <div className={classes.lineLocation}>
-            {'\u00A0 \u00A0'} @{location}
-          </div>
-        </div>
-        <div className={classes.text} dangerouslySetInnerHTML={formatXpText(text)} />
-        <div className={classes.notes}>{notes}</div>
-      </div>
-    </div>
-  );
-}
 
 function SectionXp({ intl }) {
   const classes = useStyles();
@@ -106,50 +30,14 @@ function SectionXp({ intl }) {
       </SectionTitle>
       <SectionContent collapsed={collapsed}>
         <div className={classes.content}>
-          <Line
-            key={'xp5'}
-            period="2022 - 2023"
-            role={intl.formatMessage({ id: `str.xp5.role` })}
-            location={intl.formatMessage({ id: `str.xp5.location` })}
-            text={intl.formatMessage({ id: `str.xp5.text` })}
-            notes={intl.formatMessage({ id: `str.xp5.notes` })}
-          />
-
-          <Line
-            key={'xp4'}
-            period="2020 - 2022"
-            role={intl.formatMessage({ id: `str.xp4.role` })}
-            location={intl.formatMessage({ id: `str.xp4.location` })}
-            text={intl.formatMessage({ id: `str.xp4.text` })}
-            notes={intl.formatMessage({ id: `str.xp4.notes` })}
-          />
+          <LineXp key="xp6" id="xp6" />
+          <LineXp key="xp5" id="xp5" />
+          <LineXp key="xp4" id="xp4" />
           <PrintPageBreaker />
-          <Line
-            key={'xp3'}
-            period="2019 - 2020"
-            role={intl.formatMessage({ id: `str.xp3.role` })}
-            location={intl.formatMessage({ id: `str.xp3.location` })}
-            text={intl.formatMessage({ id: `str.xp3.text` })}
-            notes={intl.formatMessage({ id: `str.xp3.notes` })}
-          />
-
-          <Line
-            key={'xp2'}
-            period="2010 - 2019"
-            role={intl.formatMessage({ id: `str.xp2.role` })}
-            location={intl.formatMessage({ id: `str.xp2.location` })}
-            text={intl.formatMessage({ id: `str.xp2.text` })}
-            notes={intl.formatMessage({ id: `str.xp2.notes` })}
-          />
+          <LineXp key="xp3" id="xp3" />
+          <LineXp key="xp2" id="xp2" />
           {/* <PrintPageBreaker /> */}
-          <Line
-            key={'xp1'}
-            period="2000 - 2009"
-            role={intl.formatMessage({ id: `str.xp1.role` })}
-            location={intl.formatMessage({ id: `str.xp1.location` })}
-            text={intl.formatMessage({ id: `str.xp1.text` })}
-            notes={intl.formatMessage({ id: `str.xp1.notes` })}
-          />
+          <LineXp key="xp1" id="xp1" />
         </div>
       </SectionContent>
     </Section>
